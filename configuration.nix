8a924897b6fbd45e6f3d7228fc50909ff97de3da
jamerman <jamerman1998@gmail.com>
2024-12-22 16:19:15 +0000

    First version where I will try using GitHub to keep everything in sync. This is also the start of using flakes instead of just configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,174 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+    hostName = "NixLaptop";
+    networkmanager.enable = true;
+  };
+
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Configure console keymap
+  console.keyMap = "dvorak";
+
+  # sound.enable = true;
+
+  services = {
+    xserver = {
+      enable = true;
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          i3status
+        ];
+      };
+      xkb = {
+        layout = "us";
+        variant = "dvorak";
+      };
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+      };
+      displayManager = {
+        lightdm.enable = true;
+        defaultSession = "xfce+i3";
+      };
+    };
+    gvfs.enable = true;
+    gnome.gnome-keyring.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      pulseaudio = true;
+    };
+  };
+
+  # Username stuff
+  users.users.james = {
+    isNormalUser = true;
+    description = "";
+    extraGroups = [ "networkmanager" "wheel" "lp" "video" "audio" ];
+  };
+
+#   home-manager.backupFileExtension = "backup";
+#   home-manager.users.james = {pkgs, ... }: {
+#     programs = {
+# 
+#           zoxide = {
+#               enable = true;
+#               enableBashIntegration = true;
+#               options = [
+#                   "--cmd z"
+#               ];
+#           };
+# 
+#     };
+#     home.stateVersion = "24.11";
+# #     home.file.".config/nvim" = { source = /home/james/nix_setup/nvim; };
+# };
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    cargo
+    clang
+    dmenu
+    feh
+    fzf
+    gcc
+    git
+    gnome.gnome-keyring
+    nerdfonts
+    networkmanagerapplet
+    nitrogen
+    pasystray
+    picom
+    polkit_gnome
+    pulseaudioFull
+    rofi
+    rustc
+    wget
+    vim
+    vivaldi
+    unrar
+    unzip
+
+#    inputs.nixvim.packages.${system}.default
+  ];
+
+  programs = {
+    thunar.enable = true;
+    dconf.enable = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+  
+  hardware = {
+    bluetooth.enable = true;
+  };
+
+ nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Don't touch this
+  system.stateVersion = "23.05";
+}

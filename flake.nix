b9472d724c0f70882f6ee64045024c697f0412c9
Jamerman <jamerman1998@gmail.com>
2024-12-24 10:12:08 +0000

    Removed dependency on dodgy flake

--- flake.nix
+++ flake.nix
@@ -5,13 +5,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "path:./modules/nvim";  # Should resolve to ./modules/nvim/flake.nix
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
